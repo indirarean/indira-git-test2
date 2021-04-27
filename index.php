5d563af741ae2ac9fefb074226ec27a5b89717c7
indirarean <indiraregita26@gmail.com>
2021-04-27 13:38:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime eum quia dolor vero ratione voluptas minima? Reprehenderit, in et deserunt ad quia culpa cupiditate consequuntur, eveniet eum fugiat ea non.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
